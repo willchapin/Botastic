342bc15506503d2dd8a6ea8ac020f6489a09fd3d
willchapin <wrchapin@gmail.com>
2013-11-28 00:30:22 -0500

    searches wikipedia for the most relevant term in suject

--- smart_bot.rb
+++ smart_bot.rb
@@ -1,7 +1,11 @@
 require 'timeout'
 require 'pry'
 
+# fix ratio!!!!!!
+
 class SmartBot < Bot
+
+  THRESHOLD = 2
   
   def process_message(message)
     subjects = message['content'].scan(/tell me about ([^\.]+)/i)
@@ -45,12 +49,20 @@ class SmartBot < Bot
   end
 
   def get_random_sentence(paragraphs, subject)
+    words = subject.split(" ")
+    long_paragraphs = paragraphs.select { |p| p.length > 50 }
+    if words.length > 1
+      variants = get_variants(words, subject)
+      target = get_final_subject(variants, long_paragraphs, subject).keys.first
+    else
+      target = subject
+    end
     paragraph = ""
     sentence = ""
-    begin 
+    begin
       Timeout::timeout(5) {
-        until /#{subject}/i.match(sentence) and paragraph.length > 50
-          paragraph = paragraphs.sample
+        until /#{target}/i.match(sentence)
+          paragraph = long_paragraphs.sample
           sentence = paragraph.split(". ").sample || ""
         end
       }
@@ -59,5 +71,24 @@ class SmartBot < Bot
     end
     
     sentence
-  end  
+  end
+  
+  def get_variants(words, subject)
+    words << words.map { |str| str[0]} .join("").upcase if words.length > 1
+    words << subject
+  end
+  
+  def get_frequency(word, paragraphs)
+    occurrences = paragraphs.map {|p| p.scan(/#{word}/).length }
+    occurrences.reduce(:+)
+  end
+  
+  def get_final_subject(variants, paragraphs, subject)
+    freq_hash = variants.map {|v| { v => get_frequency(v, paragraphs) } }.reduce {|i,h| i.merge(h)}
+    winning_variant = Hash[*freq_hash.max_by {|k,v| v}]
+    subj_freq = freq_hash.select {|k,v| k == subject }
+    ratio = winning_variant.values.first/subj_freq.values.first
+    ratio > THRESHOLD ? winning_variant : subj_freq 
+  end
+
 end
