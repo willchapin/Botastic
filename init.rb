92fed3a138f9e9ab73112f95e89b341f886bb14b
willchapin <wrchapin@gmail.com>
2013-12-03 11:52:56 -0500

    remove init from repo

--- init.rb
+++
@@ -1,5 +0,0 @@
-load('bot.rb')
-load('smart_bot.rb')
-load('message.rb')
-b = SmartBot.new({ email: 'botastic-bot@students.hackerschool.com',
-                   key: '8mnr2NGZbmDNrm7laiDrgMmi51RRW0dW' })
